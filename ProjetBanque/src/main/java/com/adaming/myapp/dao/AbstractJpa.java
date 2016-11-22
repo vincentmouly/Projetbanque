8f7552a2536f8f83ec6f4d35685425774742d5be
inti0271 <inti0271@INTI-0271>
2016-11-22 16:47:41 +0100

    commit lioc

--- ProjetBanque/src/main/java/com/adaming/myapp/dao/AbstractJpa.java
+++ ProjetBanque/src/main/java/com/adaming/myapp/dao/AbstractJpa.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
+//comments
 public class AbstractJpa<T extends Serializable> {
 	
 	@PersistenceContext
